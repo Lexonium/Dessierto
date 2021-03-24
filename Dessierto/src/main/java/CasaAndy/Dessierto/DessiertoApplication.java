ba673ee15573615cf5871fccb3a37036d17fc193
Lexonium <44877578+Lexonium@users.noreply.github.com>
2021-03-23 20:16:31 -0700

    Add files via upload
    Inicio Proyecto. Todo Vacio

---
+++ Dessierto/src/main/java/CasaAndy/Dessierto/DessiertoApplication.java
@@ -0,0 +1,13 @@
+package CasaAndy.Dessierto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DessiertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DessiertoApplication.class, args);
+	}
+
+}
